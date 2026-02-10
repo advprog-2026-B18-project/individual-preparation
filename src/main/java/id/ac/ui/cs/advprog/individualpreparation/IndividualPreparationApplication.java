ee6e7a482b1ffae98d43366b982f9ceb3146aba5
r3gulus-4rcturus <muh.lanangzalkifla@gmail.com>
2026-02-10 19:07:45 +0700

    initial commit

---
+++ src/main/java/id/ac/ui/cs/advprog/individualpreparation/IndividualPreparationApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.individualpreparation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndividualPreparationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IndividualPreparationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/individualpreparation/IndividualPreparationApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.individualpreparation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndividualPreparationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
