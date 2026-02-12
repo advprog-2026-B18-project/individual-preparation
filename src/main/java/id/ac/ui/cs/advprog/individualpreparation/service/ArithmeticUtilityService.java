f86ab45d1d3a4e328369200fd0cb3f5d65f9cc19
nayraarr <zita.nayra.ardini@gmail.com>
2026-02-12 22:46:08 +0700

    Create method service of subtract

--- src/main/java/id/ac/ui/cs/advprog/individualpreparation/service/ArithmeticUtilityService.java
+++ src/main/java/id/ac/ui/cs/advprog/individualpreparation/service/ArithmeticUtilityService.java
@@ -1,9 +1,16 @@
 package id.ac.ui.cs.advprog.individualpreparation.service;
 
+import id.ac.ui.cs.advprog.individualpreparation.model.ArithmeticResponse;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ArithmeticUtilityService {
 
-    // todo
+    public ArithmeticResponse subtract(long o1, long o2){
+        long result = o1 - o2;
+
+        ArithmeticResponse arithmeticResponse = new ArithmeticResponse();
+        arithmeticResponse.setResult(result);
+        return arithmeticResponse;
+    }
 }
