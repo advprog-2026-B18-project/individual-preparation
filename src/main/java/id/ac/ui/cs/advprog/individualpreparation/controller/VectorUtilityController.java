4abaebb76ecaf601ad8a6a724b3e570ad428b3f8
Raihana Auni Zakia <zakiaraihanauni@gmail.com>
2026-02-12 21:32:50 +0700

    Implement vector subtract

--- src/main/java/id/ac/ui/cs/advprog/individualpreparation/controller/VectorUtilityController.java
+++ src/main/java/id/ac/ui/cs/advprog/individualpreparation/controller/VectorUtilityController.java
@@ -1,18 +1,19 @@
 package id.ac.ui.cs.advprog.individualpreparation.controller;
 
+import id.ac.ui.cs.advprog.individualpreparation.dto.VectorRequest;
+import id.ac.ui.cs.advprog.individualpreparation.dto.VectorResponse;
 import id.ac.ui.cs.advprog.individualpreparation.service.VectorUtilityService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @RestController
-@RequestMapping("/api/arithmetic")
+@RequestMapping("/api/vector")
 public class VectorUtilityController {
-
     @Autowired
     private VectorUtilityService service;
 
-    // todo
-
+    @PostMapping("/subtract")
+    public VectorResponse subtract(@RequestBody VectorRequest request) {
+        return service.subtract(request.getV1(), request.getV2());
+    }
 }
--- src/main/java/id/ac/ui/cs/advprog/individualpreparation/service/VectorUtilityService.java
+++ src/main/java/id/ac/ui/cs/advprog/individualpreparation/service/VectorUtilityService.java
@@ -1,9 +1,19 @@
 package id.ac.ui.cs.advprog.individualpreparation.service;
 
+import id.ac.ui.cs.advprog.individualpreparation.dto.VectorResponse;
 import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class VectorUtilityService {
-
-    // todo
+    public VectorResponse subtract(List<Integer> v1, List<Integer> v2) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < v1.size(); i++) {
+            result.add(v1.get(i) - v2.get(i));
+        }
+        VectorResponse response = new VectorResponse();
+        response.setResult(result);
+        return response;
+    }
 }
