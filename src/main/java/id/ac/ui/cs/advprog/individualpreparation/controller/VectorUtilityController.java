d4a9ba72b4245d33fbc807c721a65fa44193f54d
nayraarr <zita.nayra.ardini@gmail.com>
2026-02-12 22:33:09 +0700

    Create dotproduct controller

--- src/main/java/id/ac/ui/cs/advprog/individualpreparation/controller/VectorUtilityController.java
+++ src/main/java/id/ac/ui/cs/advprog/individualpreparation/controller/VectorUtilityController.java
@@ -1,10 +1,15 @@
 package id.ac.ui.cs.advprog.individualpreparation.controller;
 
+import id.ac.ui.cs.advprog.individualpreparation.model.ScalarResponse;
+import id.ac.ui.cs.advprog.individualpreparation.model.VectorRequest;
+import id.ac.ui.cs.advprog.individualpreparation.model.VectorResponse;
 import id.ac.ui.cs.advprog.individualpreparation.service.VectorUtilityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/arithmetic")
@@ -13,6 +18,12 @@ public class VectorUtilityController {
     @Autowired
     private VectorUtilityService service;
 
-    // todo
+    @PostMapping("dotProduct")
+    public ScalarResponse dotproduct(@RequestBody VectorRequest request){
+        List<Integer> v1 = request.getV1();
+        List<Integer> v2 = request.getV2();
+
+        return service.dotproduct(v1, v2);
+    }
 
 }
