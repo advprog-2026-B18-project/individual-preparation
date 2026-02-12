b46db1bf2a88a689ce5242e49bdc77f2416fae0a
r3gulus-4rcturus <muh.lanangzalkifla@gmail.com>
2026-02-12 16:46:14 +0700

    implement exponent functionality on the arithmetic utility controller

--- src/main/java/id/ac/ui/cs/advprog/individualpreparation/controller/ArithmeticUtilityController.java
+++ src/main/java/id/ac/ui/cs/advprog/individualpreparation/controller/ArithmeticUtilityController.java
@@ -1,10 +1,14 @@
 package id.ac.ui.cs.advprog.individualpreparation.controller;
 
+import id.ac.ui.cs.advprog.individualpreparation.dto.ArithmeticResponse;
+import id.ac.ui.cs.advprog.individualpreparation.dto.ArithmeticRequest;
 import id.ac.ui.cs.advprog.individualpreparation.service.ArithmeticUtilityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/arithmetic")
@@ -13,6 +17,12 @@ public class ArithmeticUtilityController {
     @Autowired
     private ArithmeticUtilityService service;
 
-    // todo
+    @PostMapping("/exponent")
+    public ArithmeticResponse exponent(@RequestBody ArithmeticRequest request){
+        long o1 = request.getO1();
+        long n = request.getO2();
+
+        return service.exponent(o1, n);
+    }
 
 }
