daef6c8e1e04d02cf23db86491ccf2b68fe5f236
r3gulus-4rcturus <muh.lanangzalkifla@gmail.com>
2026-02-13 10:35:13 +0700

    add global exception handler to handle the exception on the controller

--- src/main/java/id/ac/ui/cs/advprog/individualpreparation/controller/GlobalExceptionHandler.java
+++ src/main/java/id/ac/ui/cs/advprog/individualpreparation/controller/GlobalExceptionHandler.java
@@ -1,4 +1,20 @@
 package id.ac.ui.cs.advprog.individualpreparation.controller;
 
+import id.ac.ui.cs.advprog.individualpreparation.dto.ErrorResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalArgument(
+            IllegalArgumentException ex){
+
+        ErrorResponse error =
+                new ErrorResponse(ex.getMessage(), "error");
+
+        return ResponseEntity.badRequest().body(error);
+    }
 }
